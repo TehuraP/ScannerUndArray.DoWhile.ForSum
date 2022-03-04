1835edcec58fede1a287ec07d99689e44e3155f0
emmaofthevoid <tehurapardo@outlook.es>
2022-03-04 11:50:19 +0100

    Initial commit

---
+++ src/com/company/ArithmeticTowerMethode.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class ArithmeticTower {
+    public static void main(String[] args) {
+        int number = 100;
+        long number1 = 202103;
+        System.out.println(number1);
+        makeTower(number);
+
+    }
+    public static void makeTower (int number) {
+        for (int i = 2; i < 10; i++) {
+            System.out.println(number + " * " + i);
+            number = number * i;
+        }
+        for (int j = 2 ; j < 10; j++ ){
+            System.out.println(number + "/" + j);
+            number = number/j;
+        }
+        System.out.println(number);
+    }
+}
